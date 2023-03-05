4ef2b51a76fbf778a3bb98d6118f9d796743119e
Neil Smith <neil@thisisfocus.co.uk>
2023-03-05 08:09:50 +0000

    Move to production environment for auth0

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -28,9 +28,9 @@ class Auth0Controller < ApplicationController
   def logout_url
     request_params = {
       returnTo: admin_url,
-      client_id: Rails.application.credentials.auth0.client_id
+      client_id: Rails.application.credentials.auth0[Rails.env].client_id
     }
 
-    URI::HTTPS.build(host: Rails.application.credentials.auth0.domain, path: "/v2/logout", query: request_params.to_query).to_s
+    URI::HTTPS.build(host: Rails.application.credentials.auth0[Rails.env].domain, path: "/v2/logout", query: request_params.to_query).to_s
   end
 end
