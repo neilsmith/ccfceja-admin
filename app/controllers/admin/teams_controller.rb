e459c66e3d6985ea9051412fc17c678b9958c49e
Neil Smith <neil@thisisfocus.co.uk>
2023-03-06 22:22:23 +0000

    Permit twitter handle

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -10,6 +10,6 @@ class Admin::TeamsController < Admin::BaseController
   end
 
   def clean_params
-    params.require(:team).permit(:slug, :name, :manager_1_position, :manager_1_name, :manager_2_position, :manager_2_name, :manager_3_position, :manager_3_name, :manager_4_position, :manager_4_name, :fa_table_lrcode, :fa_table_division_season, :fa_fixture_lrcode, :fa_fixture_division_season, :players_as_paragraph, :team_photo)
+    params.require(:team).permit(:slug, :name, :manager_1_position, :manager_1_name, :manager_2_position, :manager_2_name, :manager_3_position, :manager_3_name, :manager_4_position, :manager_4_name, :fa_table_lrcode, :fa_table_division_season, :fa_fixture_lrcode, :fa_fixture_division_season, :players_as_paragraph, :team_photo, :twitter_handle)
   end
 end
